6ecd9363ae6a6f46eec4c345c07dbe341011d912
hypering <hypering@github.com>
2023-02-09 19:03:16 +0900

    init: 초기 설정

---
+++ src/main/kotlin/com/schedule/manager/schedulemanager/ScheduleManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.schedule.manager.schedulemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduleManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScheduleManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/schedule/manager/schedulemanager/ScheduleManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.schedule.manager.schedulemanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScheduleManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
